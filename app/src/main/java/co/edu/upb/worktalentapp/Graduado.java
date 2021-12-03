28d39ac618f4e3f7afc3b9588f759d49a6e7925b
mynarvaez9 <mynarvaez9@misena.edu.co>
2021-12-03 18:27:11 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/upb/worktalentapp/Graduado.java
@@ -0,0 +1,14 @@
+package co.edu.upb.worktalentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Graduado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graduado);
+    }
+}
